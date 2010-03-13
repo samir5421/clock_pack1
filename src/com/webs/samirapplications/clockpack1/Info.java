9b1a8b8a214586e5be99b740b73c14b8f12814c3
Samir <samir314159@gmail.com>
2010-03-12 20:24:28 -0600

    first commit

---
+++ src/com/webs/samirapplications/clockpack1/Info.java
@@ -0,0 +1,13 @@
+package com.webs.samirapplications.clockpack1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState); setContentView(R.layout.info);
+    }
+}
